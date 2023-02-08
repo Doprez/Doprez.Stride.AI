ad0247125a1135d19e170fd859846d3273430ac2
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 21:19:41 -0700

    Added base for an FSM

--- Doprez.Stride.AI/Doprez.Stride.AI/FSMs/FSM.cs
+++ Doprez.Stride.AI/Doprez.Stride.AI/FSMs/FSM.cs
@@ -1,4 +1,5 @@
-﻿using Stride.Engine;
+﻿using Stride.Core;
+using Stride.Engine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,67 @@ namespace Doprez.Stride.AI.FSMs;
 /// <summary>
 /// Useless for the moment
 /// </summary>
-public class FSM : AsyncScript
+public abstract class FSM : AsyncScript
 {
-	public override Task Execute()
+	/// <summary>
+	/// All states that can be accessed by the FSM
+	/// </summary>
+	[DataMemberIgnore]
+	public Dictionary<int, FSMState> States = new Dictionary<int, FSMState>();
+
+	protected FSMState _currentState;
+
+	public void Add(int id, FSMState state)
 	{
-		throw new NotImplementedException();
+		States.Add(id, state);
 	}
+
+	public override async Task Execute()
+	{
+		while (Game.IsRunning)
+		{
+			await _currentState.UpdateState();
+		}
+	}
+
+	public FSMState GetActiveState()
+	{
+		return _currentState;
+	}
+
+	public FSMState GetState(int id)
+	{
+		return States[id];
+	}
+
+	public void SetCurrentState(FSMState state)
+	{
+		if (_currentState != null)
+		{
+			_currentState.ExitState();
+		}
+
+		_currentState = state;
+
+		if (_currentState != null)
+		{
+			_currentState.EnterState();
+		}
+	}
+
+	public void SetCurrentState(int stateIndex)
+	{
+		if (_currentState != null)
+		{
+			_currentState.ExitState();
+		}
+
+		_currentState = GetState(stateIndex);
+
+		if (_currentState != null)
+		{
+			_currentState.EnterState();
+		}
+	}
+
 }
---
+++ Doprez.Stride.AI/Doprez.Stride.AI/FSMs/FSMState.cs
@@ -0,0 +1,19 @@
+﻿using Stride.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doprez.Stride.AI.FSMs;
+public abstract class FSMState : StartupScript
+{
+	public string Name { get; set; } = Guid.NewGuid().ToString();
+	public FSM FiniteStateMachine { get; set; }
+	public bool IsDefaultState { get; set; }
+
+	public abstract Task EnterState();
+	public abstract Task ExitState();
+	public abstract Task UpdateState();
+
+}
