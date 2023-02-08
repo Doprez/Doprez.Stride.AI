fd2dec2e052c14c860aa4e9c43ec5596dc54c666
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 21:19:25 -0700

    Added Plan method to GoapAgent Untested and could be made more efficient Im sure

--- Doprez.Stride.AI/Doprez.Stride.AI/GOAP/GoapAgent.cs
+++ Doprez.Stride.AI/Doprez.Stride.AI/GOAP/GoapAgent.cs
@@ -1,4 +1,5 @@
-﻿using Stride.Core;
+﻿using BulletSharp;
+using Stride.Core;
 using Stride.Engine;
 using System;
 using System.Collections.Generic;
@@ -11,11 +12,93 @@ public abstract class GoapAgent : AsyncScript
 {
 	[DataMember(0)]
 	public string AgentName { get; set; } = string.Empty;
-
 	//Need list of Actions To be performed
 	public List<GoapAction> Actions { get; set; } = new();
-
 	//need input state that should be modified
 	public GoapState State { get; set; } = new();
+
+	private Queue<GoapAction>? _plan;
+
+
+	public Queue<GoapAction> Plan(Dictionary<string, bool> worldState, Dictionary<string, bool> goal)
+	{
+		// reset the plan
+		_plan.Clear();
+
+		// Create a new list of actions to check
+		List<GoapAction> usableActions = new List<GoapAction>(Actions);
+
+		// Check each action to see if its preconditions are met
+		foreach (GoapAction action in usableActions)
+		{
+			action.Reset();
+			foreach (KeyValuePair<string, bool> precondition in action.PreConditions)
+			{
+				if (worldState.ContainsKey(precondition.Key) && worldState[precondition.Key] == precondition.Value)
+				{
+					action.IsUsable = true;
+				}
+				else
+				{
+					action.IsUsable = false;
+					break;
+				}
+			}
+		}
+
+		// Sort the list of usable actions
+		usableActions.Sort((x, y) => x.Cost.CompareTo(y.Cost));
+
+		// Create a dictionary to store the state of the world after each action
+		Dictionary<string, bool> currentState = new Dictionary<string, bool>(worldState);
+
+		// Create a list to store the actions taken
+		List<GoapAction> actionPlan = new List<GoapAction>();
+
+		// loop through the list of usable actions and try to achieve the goal
+		foreach (GoapAction action in usableActions)
+		{
+			// Check if the action can be performed
+			if (action.IsUsable)
+			{
+				// Check if the goal has been met
+				bool goalMet = true;
+				foreach (KeyValuePair<string, bool> goalState in goal)
+				{
+					if (currentState.ContainsKey(goalState.Key) && currentState[goalState.Key] == goalState.Value)
+					{
+						continue;
+					}
+					else
+					{
+						goalMet = false;
+						break;
+					}
+				}
+
+				// If the goal has been met, return the plan
+				if (goalMet)
+				{
+					foreach (GoapAction a in actionPlan)
+					{
+						_plan.Enqueue(a);
+					}
+					return _plan;
+				}
+				else
+				{
+					// Apply the effects of the action to the current state
+					foreach (KeyValuePair<string, bool> effect in action.PostConditions)
+					{
+						currentState[effect.Key] = effect.Value;
+					}
+					actionPlan.Add(action);
+				}
+			}
+		}
+
+		// If a plan cannot be found, return an empty queue
+		return new Queue<GoapAction>();
+	}
 
 }
