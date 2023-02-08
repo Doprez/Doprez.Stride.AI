1754d1458db289f54d234c2687512d2ab536d29b
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 21:20:14 -0700

    Added some extra class vars for GoapAction usage

--- Doprez.Stride.AI/Doprez.Stride.AI/GOAP/GoapAction.cs
+++ Doprez.Stride.AI/Doprez.Stride.AI/GOAP/GoapAction.cs
@@ -3,13 +3,21 @@
 namespace Doprez.Stride.AI.GOAP;
 public abstract class GoapAction : StartupScript
 {
-
+	public string Name = Guid.NewGuid().ToString();
+	public int Cost;
 	public Dictionary<string, bool> PreConditions = new();
 	public Dictionary<string, bool> PostConditions = new();
+	public bool IsUsable;
 
 	/// <summary>
 	/// Returns true if the action was successful
 	/// </summary>
 	/// <returns></returns>
 	public abstract Task<bool> RunAction();
+
+	public void Reset()
+	{
+		IsUsable = false;
+	}
+
 }
