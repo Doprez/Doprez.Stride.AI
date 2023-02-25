fc4d824e6a5cf4c575f4cafef5d922d4dccf9e94
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-24 20:07:58 -0700

    change FSM to AsyncFSM

---
+++ Doprez.Stride.AI/Doprez.Stride.AI/FSMs/AsyncFSM.cs
@@ -0,0 +1,65 @@
+﻿using Stride.Core;
+using Stride.Engine;
+
+namespace Doprez.Stride.AI.FSMs;
+
+/// <summary>
+/// Useless for the moment
+/// </summary>
+public abstract class AsyncFSM : AsyncScript
+{
+	/// <summary>
+	/// All states that can be accessed by the FSM
+	/// </summary>
+	[DataMemberIgnore]
+	public Dictionary<int, AsyncFSMState> States = new();
+
+	protected AsyncFSMState? currentState;
+
+	public void Add(int id, AsyncFSMState state)
+	{
+		States.Add(id, state);
+	}
+
+	public abstract void Initialize();
+	public abstract Task AsyncUpdate();
+
+	public override async Task Execute()
+	{
+		Initialize();
+		while (Game.IsRunning)
+		{
+			await AsyncUpdate();
+			await currentState.UpdateState();
+		}
+	}
+
+	public AsyncFSMState GetActiveState()
+	{
+		return currentState;
+	}
+
+	public AsyncFSMState GetState(int id)
+	{
+		return States[id];
+	}
+
+	public void SetCurrentState(AsyncFSMState state)
+	{
+		currentState?.ExitState();
+
+		currentState = state;
+
+		currentState?.EnterState();
+	}
+
+	public void SetCurrentState(int stateIndex)
+	{
+		currentState?.ExitState();
+
+		currentState = GetState(stateIndex);
+
+		currentState?.EnterState();
+	}
+
+}
---
+++ Doprez.Stride.AI/Doprez.Stride.AI/FSMs/AsyncFSMState.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Doprez.Stride.AI.FSMs;
+public abstract class AsyncFSMState
+{
+	public string Name { get; set; } = Guid.NewGuid().ToString();
+	public AsyncFSM? FiniteStateMachine { get; set; }
+	public bool IsDefaultState { get; set; }
+
+	public abstract Task EnterState();
+	public abstract Task ExitState();
+	public abstract Task UpdateState();
+
+}
