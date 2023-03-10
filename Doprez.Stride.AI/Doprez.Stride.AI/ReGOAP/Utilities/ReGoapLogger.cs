28310f4dfa3fce3a6999db3b9efc06e269efeea0
Doprez <73259914+Doprez@users.noreply.github.com>
2023-03-10 09:30:43 -0700

    added core for Regoap implementation

---
+++ Doprez.Stride.AI/Doprez.Stride.AI/ReGOAP/Utilities/ReGoapLogger.cs
@@ -0,0 +1,69 @@
+﻿#define DEBUG
+using System;
+
+namespace ReGoap.Utilities
+{
+    public static class ReGoapLogger
+    {
+    private class GenericTraceListener : IListener
+    {
+        public void Write(string message)
+        {
+            Write(message, "");
+        }
+
+        public void Write(string message, string category)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+        [Flags]
+        public enum DebugLevel
+        {
+            None, ErrorsOnly, WarningsOnly, Full
+        }
+        public static DebugLevel Level = DebugLevel.Full;
+        public static bool RunOnlyOnMainThread = true;
+
+        private static readonly IListener listener;
+
+        private static readonly int mainThreadId;
+
+        static ReGoapLogger()
+        {
+            mainThreadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+            listener = new GenericTraceListener();
+        }
+
+        private static bool InMainThread()
+        {
+            return !RunOnlyOnMainThread || System.Threading.Thread.CurrentThread.ManagedThreadId == mainThreadId;
+        }
+
+        public static void Log(string message)
+        {
+            if (Level != DebugLevel.Full || !InMainThread()) return;
+            listener.Write(message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            if (Level < DebugLevel.WarningsOnly || !InMainThread()) return;
+            listener.Write(message, "warning");
+        }
+
+        public static void LogError(string message)
+        {
+            if (Level < DebugLevel.ErrorsOnly || !InMainThread()) return;
+            listener.Write(message, "error");
+        }
+    }
+
+    internal interface IListener
+    {
+        void Write(string text);
+        void Write(string text, string category);
+    }
+}
